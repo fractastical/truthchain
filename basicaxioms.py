22288227ebe71d76824eac004265495748c74458
Joel Dietz <jdietz+github@gmail.com>
2024-05-06 13:01:02 +0200

    reorder

--- basicaxioms.py
+++ basicaxioms.py
@@ -9,11 +9,12 @@ G = nx.DiGraph()
 
 
 # Add nodes (axioms, beliefs, and definitions)
-G.add_node("There is order to the universe", type="axiom")
+G.add_node("()", type="axiom")
+# G.add_node("There is order to the universe", type="axiom")
 G.add_node("I can understand the order", type="axiom")
 G.add_node("Data input varies in reliability", type="axiom")
 G.add_node("I can add data by querying third parties", type="axiom")
-G.add_node("()", type="axiom")
+G.add_node("There is order to the universe", type="axiom")
 G.add_node("I need to continually reassess the reliability of this data", type="belief")
 G.add_node("My base sensory data is somewhat reliable", type="belief")
 G.add_node("If you score the data reliability along a linear axis this will produce more reliable results than binary data", type="belief")
@@ -28,30 +29,17 @@ G.add_edge("Data input varies in reliability", "I need to continually reassess t
 G.add_edge("I can add data by querying third parties", "This data can be aggregated")
 G.add_edge("This data can be aggregated", "If you score the data reliability along a linear axis this will produce more reliable results than binary data")
 
-# G.add_edge("This data can be aggregated", "We call this phenomenological scope")
 
-# Add nodes (axioms, beliefs, and definitions)
-# G.add_node("There is order to the universe", type="axiom")
-# G.add_node("I can understand the order", type="axiom")
-# G.add_node("Data input varies in reliability", type="axiom")
-# G.add_node("I can add data by querying third parties", type="axiom")
-# G.add_node("empty", type="axiom")
-# G.add_node("I need to continually reassess the reliability of this data", type="belief")
-# G.add_node("My base sensory data is somewhat reliable", type="belief")
-# G.add_node("If you score the data reliability along a linear axis this will produce more reliable results than binary data", type="belief")
 
-# # G.add_node("Score", type="belief")
-# G.add_node("This aggregated data is known as phenomenological scope", type="belief")
+# Human social dynamics
 
-# # G.add_node("This aggregated data is known as phenomenological scope", type="definition")
 
-# # Add edges (relationships)
-# G.add_edge("There is order to the universe", "My base sensory data is somewhat reliable")
-# G.add_edge("I can add data by querying third parties", "This aggregated data is known as phenomenological scope")
-# G.add_edge("Data input varies in reliability", "I need to continually reassess the reliability of this data")
+
 
 # Create a custom layout
-pos = {}
+# pos = {}
+pos = nx.spring_layout(G, scale=2)  # scale parameter to spread nodes further apart
+
 axioms = [node for node in G.nodes() if G.nodes[node]['type'] == 'axiom']
 beliefs = [node for node in G.nodes() if G.nodes[node]['type'] == 'belief']
 definitions = [node for node in G.nodes() if G.nodes[node]['type'] == 'definition']
@@ -81,7 +69,7 @@ nx.draw_networkx_nodes(G, pos, node_size=4000, node_color=node_colors)  # Increa
 nx.draw_networkx_edges(G, pos, edge_color="gray", arrows=True)
 
 # Draw labels with line wrapping
-labels = {node: textwrap.fill(node, width=40) for node in G.nodes()}  # Increase the width for line wrapping
+labels = {node: textwrap.fill(node, width=20) for node in G.nodes()}  # Adjust width as needed
 nx.draw_networkx_labels(G, pos, labels, font_size=8, font_weight="bold", horizontalalignment='left', verticalalignment='center')
 
 # Create a legend
